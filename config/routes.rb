1a1eed918965dd2878781369ff44e50a8aa8b98e
mdvitalie.ie <mdvitalie@gmail.com>
2022-11-18 12:28:24 +0000

    first commit -Book, partials, nav

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :books
+  #get 'static_page/home'
+  root to: 'static_page#home'
+  #get 'static_page/about'
+  get '/about', to: 'static_page#about' 
+  #get 'static_page/contact'
+  get '/contact', to: 'static_page#contact'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
---
+++ db/migrate/20221104140117_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.text :title
+      t.string :author
+      t.integer :quantity
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
